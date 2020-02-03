b5796718a43fe83170c058bfe8627ae0e6810e0b
Richmond <rbaafi@cnm.edu>
2020-02-03 11:54:43 -0700

    Tested for affirmative.csv and negative.csv.

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -7,13 +7,13 @@ public final class Palindromes {
   private static boolean isPalindromeClean(String source) {
     return source.length() <= 1
         || (
-            source.charAt(0) == source.charAt(source.length() - 1)
-            && isPalindromeClean(source.substring(1, source.length() - 1).toLowerCase())
-        );
+        source.charAt(0) == source.charAt(source.length() - 1)
+            && isPalindromeClean(source.substring(1, source.length() - 1))
+    );
   }
 
   public static boolean isPalindrome(String source) {
-    return isPalindromeClean(source.replaceAll("[\\W_]+", ""));
+    return isPalindromeClean(source.replaceAll("[\\W_]+", "").toLowerCase());
   }
 
 }
